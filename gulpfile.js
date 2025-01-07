a969377a2162879eb67065e1e4dd834fcd4f536b
Kazak <strannik19742@mail.ru>
2025-01-07 13:16:00 +0300

    Фильтр работает

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
+
+
+function  buildStyles ( )  { 
+  return  gulp.src ( ' ./sass/**/*.scss ' ) .pipe ( sass ( ) .on ( ' error ' , sass.logError ) ) .pipe ( gulp.dest ( ' ./css ' ) ) ; } ; exports.buildStyles = buildStyles ; exports.watch = function ( ) { gulp.watch ( ' ./sass/**/*.scss ' , buildStyles ) ; } ;
