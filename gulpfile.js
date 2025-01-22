5c180d4cd6c842b2b6229fde932ad9e7f486f414
Kazak <strannik19742@mail.ru>
2025-01-22 19:16:01 +0300

    Ending

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
+const gulp = require('gulp');
+const dartSass = require('sass');
+const gulpSass = require('gulp-sass');
 const sass = gulpSass(dartSass);
 
+// Задача для компиляции SCSS в CSS
+function buildStyles() {
+  return gulp
+    .src('./src/sass/**/*.sass') // Исходные SCSS файлы
+    .pipe(sass().on('error', sass.logError)) // Компиляция SCSS
+    .pipe(gulp.dest('./css')); // Сохранение в папку
+}
 
-function  buildStyles ( )  { 
-  return  gulp.src ( ' ./sass/**/*.scss ' ) .pipe ( sass ( ) .on ( ' error ' , sass.logError ) ) .pipe ( gulp.dest ( ' ./css ' ) ) ; } ; exports.buildStyles = buildStyles ; exports.watch = function ( ) { gulp.watch ( ' ./sass/**/*.scss ' , buildStyles ) ; } ;
+// Задача для отслеживания изменений
+function watchFiles() {
+  gulp.watch('./src/sass/**/*.sass', buildStyles); // Отслеживание SCSS файлов
+}
+
+// Экспортируем задачи
+exports.buildStyles = buildStyles;
+exports.watch = gulp.series(buildStyles, watchFiles); // Последовательное выполнение задач
